805cbca3fc6cc05831a9b19e1c36fc4487db6f84
prettykernel <prettykernel@gmail.com>
2019-08-18 15:35:10 +0800

    dev

---
+++ myapp/pages/index.js
@@ -0,0 +1,12 @@
+
+import styles from './index.css';
+
+export default function() {
+  return (
+    <div className={styles.normal}>
+      <h1>Page index...</h1>
+      <h6>Page index </h6>
+      <h6>Page index </h6>
+    </div>
+  );
+}
